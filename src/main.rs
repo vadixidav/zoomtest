f23d7c8cd73bab073ce1e7f3bf4c90d7e1b7d993
vadix <vadixidav@gmail.com>
2016-01-16 23:07:12 -0500

    bumping version; crossbeam support fairly final

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,7 @@ extern crate zoom;
 extern crate num;
 extern crate nalgebra as na;
 extern crate rand;
+extern crate crossbeam;
 
 use rand::{SeedableRng, Rng};
 use kiss3d::window::Window;
@@ -11,10 +12,6 @@ use kiss3d::scene::SceneNode;
 use zoom::*;
 use num::Zero;
 use na::Vec3 as NaVec;
-
-//Threading imports
-use std::sync::mpsc;
-use std::thread;
 
 type Vec3 = Cartesian3<f64>;
 
@@ -54,89 +51,75 @@ impl Quanta<f64> for GravityPhysics {
 
 impl PhysicsParticle<Vec3, f64> for GravityPhysics {}
 
-struct Ball {
-    position: Vec3,
-    velocity: Vec3,
-    acceleration: Vec3,
+struct Thing {
+    particle: BasicParticle<Vec3, f64>,
 }
 
 fn to_navec(v: Vec3) -> NaVec<f32> {
     NaVec::new(v.x as f32, v.y as f32, v.z as f32)
 }
 
-impl Ball {
+impl Thing {
     fn new(position: Vec3, velocity: Vec3) -> Self {
-        Ball{
-            position: position,
-            velocity: velocity,
-            acceleration: Vec3::zero(),
+        Thing{
+            particle: BasicParticle::new(1.0, position, velocity, 100000.0)
         }
     }
 }
 
-impl UniformBall<f64> for GravityPhysics {
+impl Ball<f64> for GravityPhysics {
     fn radius(&self) -> f64 {
         0.1
     }
-
-    fn radius_squared(&self) -> f64 {
-        0.001
-    }
 }
 
-impl UniformBall<f64> for LorentzPhysics {
+impl Ball<f64> for LorentzPhysics {
     fn radius(&self) -> f64 {
         0.1
     }
-
-    fn radius_squared(&self) -> f64 {
-        0.001
-    }
 }
 
-impl Position<Vec3> for Ball {
+impl Position<Vec3> for Thing {
     fn position(&self) -> Vec3 {
-        self.position
+        self.particle.position()
     }
 }
 
-impl Velocity<Vec3> for Ball {
+impl Velocity<Vec3> for Thing {
     fn velocity(&self) -> Vec3 {
-        self.velocity
+        self.particle.velocity()
     }
 }
 
-impl Particle<Vec3, f64> for Ball {
-    fn accelerate(&mut self, vec: &Vec3) {
-        self.acceleration = self.acceleration + *vec;
+impl Particle<Vec3, f64> for Thing {
+    fn impulse(&self, vec: &Vec3) {
+        self.particle.impulse(vec);
     }
 
     fn advance(&mut self, time: f64) {
-        self.velocity = self.velocity + self.acceleration * time;
-        self.position = self.position + self.velocity * time;
-        self.acceleration = Vec3::zero();
+        self.particle.advance(time);
     }
 }
 
-impl Inertia<f64> for Ball {
+impl Inertia<f64> for Thing {
     fn inertia(&self) -> f64 {
-        100000.0
+        self.particle.inertia()
     }
 }
 
-impl GravityPhysics for Ball {
+impl GravityPhysics for Thing {
     fn quanta(&self) -> f64 {
         10.0
     }
 }
 
-impl LorentzPhysics for Ball {
+impl LorentzPhysics for Thing {
     fn quanta(&self) -> f64 {
         200.0
     }
 }
 
-impl SpringPhysics for Ball {
+impl SpringPhysics for Thing {
     fn quanta(&self) -> f64 {
         20.0
     }
@@ -146,61 +129,55 @@ fn main() {
     let mut window = Window::new("zoom test");
     struct SphereBall {
         scene_node: SceneNode,
-        ball: Ball,
-    }
-    #[derive(Clone)]
-    struct SBPointer {
-        pointer: *mut SphereBall,
+        ball: Thing,
     }
     unsafe impl Sync for SphereBall {}
     unsafe impl Send for SphereBall {}
-    unsafe impl Send for SBPointer {}
-    impl Copy for SBPointer {}
     let mut rng = rand::Isaac64Rng::from_seed(&[1, 3, 3, 4]);
     let mut sballs = (0..2000).map(|_| SphereBall{
         scene_node: window.add_sphere(0.2)/*window.add_cube(0.2, 0.2, 0.2)*/,
-        ball: Ball::new(Vec3::new(rng.next_f64() - 0.5, rng.next_f64() - 0.5, rng.next_f64() - 0.5) * 10.0,
+        ball: Thing::new(Vec3::new(rng.next_f64() - 0.5, rng.next_f64() - 0.5, rng.next_f64() - 0.5) * 10.0,
             Vec3::zero())
     }).map(|mut sball| {sball.scene_node.set_color(0.0, 0.0, 1.0); sball}).collect::<Vec<_>>();
 
     window.set_light(Light::StickToCamera);
 
-    let threadTotal = 4;
+    let thread_total = 4;
 
     while window.render() {
-        let (tx, rx) = mpsc::channel();
-        let len = sballs.len();
-        let sballptr = SBPointer{pointer: sballs.as_mut_ptr()};
-        for t in 0..threadTotal {
-            let tx = tx.clone();
-            thread::spawn(move || {
-                for i in (len*t/threadTotal)..(len*(t+1)/threadTotal) {
-                    unsafe {
-                        SpringPhysics::hooke_to::<SpringPhysics>(&mut (*sballptr.pointer.offset(i as isize)).ball,
-                            &(*sballptr.pointer.offset(((i - 1 + len) % len) as isize)).ball, 2.0);
-                        SpringPhysics::hooke_to::<SpringPhysics>(&mut (*sballptr.pointer.offset(i as isize)).ball,
-                            &(*sballptr.pointer.offset(((i + 1 + len) % len) as isize)).ball, 2.0);
-                        SpringPhysics::hooke_to::<SpringPhysics>(&mut (*sballptr.pointer.offset(i as isize)).ball,
-                            &(*sballptr.pointer.offset(((i + len/7 + len) % len) as isize)).ball, 2.0);
-                    }
+        crossbeam::scope(|scope| {
+            let len = sballs.len();
+            let sballs = &sballs;
+            let handles = (0..thread_total).map(|t| {
+                scope.spawn(move || {
+                    for i in (len*t/thread_total)..(len*(t+1)/thread_total) {
+                        unsafe {
+                            SpringPhysics::hooke_to::<SpringPhysics>(&(*sballs.as_ptr().offset(i as isize)).ball,
+                                &(*sballs.as_ptr().offset(((i + len - 1) % len) as isize)).ball, 2.0);
+                            SpringPhysics::hooke_to::<SpringPhysics>(&(*sballs.as_ptr().offset(i as isize)).ball,
+                                &(*sballs.as_ptr().offset(((i + 1 + len) % len) as isize)).ball, 2.0);
+                            SpringPhysics::hooke_to::<SpringPhysics>(&(*sballs.as_ptr().offset(i as isize)).ball,
+                                &(*sballs.as_ptr().offset(((i + len/7 + len) % len) as isize)).ball, 2.0);
+                        }
 
-                    for j in 0..len {
-                        if i != j {
-                            unsafe {
-                                GravityPhysics::gravitate_radius_to::<GravityPhysics>(&mut (*sballptr.pointer.offset(i as isize)).ball,
-                                    &(*sballptr.pointer.offset(j as isize)).ball, -10.0);
-                                LorentzPhysics::lorentz_radius_to::<LorentzPhysics>(&mut (*sballptr.pointer.offset(i as isize)).ball,
-                                    &(*sballptr.pointer.offset(j as isize)).ball, 0.05);
+                        for j in 0..len {
+                            if i != j {
+                                unsafe {
+                                    GravityPhysics::gravitate_radius_to::<GravityPhysics>(&(*sballs.as_ptr().offset(i as isize)).ball,
+                                        &(*sballs.as_ptr().offset(j as isize)).ball, -10.0);
+                                    LorentzPhysics::lorentz_radius_to::<LorentzPhysics>(&(*sballs.as_ptr().offset(i as isize)).ball,
+                                        &(*sballs.as_ptr().offset(j as isize)).ball, 0.05);
+                                }
                             }
                         }
                     }
-                    tx.send(());
-                }
-            });
-        }
-        for _ in 0..threadTotal {
-            let _ = rx.recv();
-        }
+                })
+            }).collect::<Vec<_>>();
+
+            for handle in handles {
+                handle.join();
+            }
+        });
         for sball in sballs.iter_mut() {
             GravityPhysics::drag(&mut sball.ball, 30000.0);
             sball.ball.advance(0.5);
